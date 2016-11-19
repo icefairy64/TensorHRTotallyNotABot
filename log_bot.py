2729ec2a75bf3eda48431a67b2469aaaadd02a28
Murka44 <roma_maurin@mail.ru>
2016-11-19 13:12:50 +0300

    Add files via upload

--- log_bot.py
+++ log_bot.py
@@ -1,14 +1,28 @@
 def create_new_user(id,name,surname):
     import os
     y=os.getcwd()+'/info_candidats'
+    create_new(y)
+    b=os.getcwd()+'/info_candidats/'+id
+    create_new(b)
+    f=file(name+'_'+surname+'_perepiska'+'.html','wb')
+    write_characteristic(f)
+def get_info_user(id):
+    f = file('*_perepiska.html')
+    str="</BODY></HTML>"
+    f.write("<b>" + str + "</br>")
+    f.close()
+def write_characteristic(id,message):
+    str = "<HTML><BODY><TITLE>" + Name + " " + surname + "<br><\TITlE>"
+    f.write(str)
+    f.close()
+def write_message(id,message):
+    f=file('*_perepiska.html')
+    f.write("<b>"+str+"=>"+"</b>")
+    f.close()
+def write_answer(id,message):
+    f=file('*_perepiska.html')
+    f.write("<b>"+str+"</br>")
+    f.close()
+def create_new(y):
     print(y)
     os.mkdir(y)
-    b=os.getcwd()+'/info_cadidats/'+
-def get_info_user(id):
-    print('пустышка')
-def write_characteristic(id,message):
-    print('пустышка')
-def write_message(id,message):
-    print('пустышка')
-def write_answer(id,message):
-    print('пустышка')
