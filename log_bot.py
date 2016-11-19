582328216ee8db3a812fc94c68877d9d85ae50a7
Murka44 <roma_maurin@mail.ru>
2016-11-19 23:29:48 +0300

    Add files via upload

--- log_bot.py
+++ log_bot.py
@@ -14,26 +14,21 @@ def create_new_user(id,name,surname):
 def get_info_user(id):
     folder = 'info_candidats/' + id + "/"
 
-    file_info = file(folder + id + '_perepiska.html','a')
+    file_info = file(folder + 'perepiska.html','a')
     str="</BODY></HTML>"
     file_info.write("<b>" + str + "</br>")
     file_info.close()
-
-    file_charect = file(folder + id + '_characteristic' + '.html', 'a')
-    str = "</BODY></HTML>"
-    file_charect.write(str)
-    file_charect.close()
 
     file_name = open(folder + id + '.txt', 'r')
     for line in file_name:
          print(line)
 
-    command_1 = "athenapdf/athenapdf " + folder + id + '_perepiska.html ' + folder + line + '_info.pdf'
+    command_1 = "athenapdf/athenapdf " + folder + 'perepiska.html ' + folder + line + '_info.pdf'
     print(command_1 )
 
     subprocess.call(command_1, shell=True)
 
-    command_2 ="athenapdf/athenapdf " + folder + id + '_characteristic.html ' + folder + line + '_charect.pdf'
+    command_2 ="athenapdf/athenapdf " + folder +  'cv.html ' + folder + line + '_charect.pdf'
     print(command_2)
 
     subprocess.call(command_2, shell=True)
@@ -47,20 +42,21 @@ def get_info_user(id):
     z.write(filename, line + '_charect.pdf')
     z.close()
 
-def write_characteristic(id,message):
-    file_charect = file(id + name + surname + '_characteristic' + '.html', 'a')
-    file_charect.write("<b>" + message +"</br>")
+def write_characteristic(id,cv):
+    folder = 'info_candidats/' + id + "/"
+    file_charect = file(folder + 'cv.html', 'wb')
+    file_charect.write(cv)
     file_charect.close()
 
 def write_message(id,message):
     y = os.getcwd() + '/info_candidats/'
-    file_info = file(y + id + "/" + id + '_perepiska.html','a')
+    file_info = file(y + id + "/" + 'perepiska.html','a')
     file_info.write("<b>" + message + "=>" + "</b>")
     file_info.close()
 
 def write_answer(id,message):
     y = os.getcwd() + '/info_candidats/'
-    file_info = file(y + id + "/" + id + '_perepiska.html','a')
+    file_info = file(y + id + "/" + 'perepiska.html','a')
     file_info.write("<b>" + message + "</br>")
     file_info.close()
 
@@ -80,13 +76,11 @@ def new_user(id,name,surname):
     id_file.close()
 
     #Создаем html с перепиской пользователя
-    file_info = file(folder + id + '_perepiska' + '.html', 'a')
+    file_info = file(folder + 'perepiska' + '.html', 'a')
     str = "<HTML><BODY><TITLE>" + name + "_" + surname + "<br><\TITlE>"
     file_info.write(str)
     file_info.close()
 
     # Создаем html с характеристикой на пользователя
-    file_charect = file(folder + id + '_characteristic' + '.html', 'a')
-    str = "<HTML><BODY><TITLE>" + name + "_" + surname + "<br><\TITlE>"
-    file_charect.write(str)
+    file_charect = file(folder + 'cv.html', 'a')
     file_charect.close()
