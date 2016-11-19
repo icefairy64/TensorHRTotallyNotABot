66dfae24ad9e07e2ba36946a455f58b0de6d0655
wuzzupall98 <wuzzupall98@gmail.com>
2016-11-19 12:23:48 +0300

    добавлен парсер строки

---
+++ parsing.py
@@ -0,0 +1,30 @@
+﻿import pymorphy2
+import re
+
+
+def split_string(string):
+    # Отправка на обработку опечаток
+    dic_typos = correct(string)
+
+    # Cоздание списка из слов строки, без знаков пунктуации
+    words = re.split('\W+', string)
+
+    # Формируем выходной список
+    return_list = []
+    
+    # Добавляем исправленные опечатки
+    for word in words:
+        if word in dic_typos:
+            for typo in dic_typos[word]:
+                return_list.append(morph.parse(typo)[0].normal_form)
+        elif word != '':
+            return_list.append(morph.parse(word)[0].normal_form)
+
+    return set(return_list)
+
+def correct(string):
+    return {'роботать': ['работать'], 'опичатка': ['опечатка', 'печатка']}
+    
+
+morph = pymorphy2.MorphAnalyzer()
+print(split_string('«дравствуйте! ћен¤ зовут ѕет¤. я бы хотел роботать в вашей компании программистом. “ут опичатка'))
