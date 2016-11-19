4e0800b121ec49345a8315c651c270331a608d1c
Murka44 <roma_maurin@mail.ru>
2016-11-19 13:21:32 +0300

    Add files via upload

--- log_bot.py
+++ log_bot.py
@@ -5,23 +5,23 @@ def create_new_user(id,name,surname):
     b=os.getcwd()+'/info_candidats/'+id
     create_new(b)
     f=file(name+'_'+surname+'_perepiska'+'.html','wb')
-    write_characteristic(f)
+    str = "<HTML><BODY><TITLE>" + Name + " " + surname + "<br><\TITlE>"
+    f.write(str)
+    f.close()
 def get_info_user(id):
-    f = file('*_perepiska.html')
+    f = file(id+'*_perepiska.html')
     str="</BODY></HTML>"
     f.write("<b>" + str + "</br>")
     f.close()
 def write_characteristic(id,message):
-    str = "<HTML><BODY><TITLE>" + Name + " " + surname + "<br><\TITlE>"
-    f.write(str)
-    f.close()
+   print('pusto')
 def write_message(id,message):
-    f=file('*_perepiska.html')
-    f.write("<b>"+str+"=>"+"</b>")
+    f=file(id+'*_perepiska.html')
+    f.write("<b>"+message+"=>"+"</b>")
     f.close()
 def write_answer(id,message):
-    f=file('*_perepiska.html')
-    f.write("<b>"+str+"</br>")
+    f=file(id+'*_perepiska.html')
+    f.write("<b>"+message+"</br>")
     f.close()
 def create_new(y):
     print(y)
