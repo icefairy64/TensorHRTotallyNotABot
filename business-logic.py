a7e3895d8e386d3ddbe09f4cd630b58d5a1c5006
wuzzupall98 <wuzzupall98@gmail.com>
2016-11-19 10:01:16 +0300

    разбиение строки на слова

--- business-logic.py
+++ business-logic.py
@@ -1,8 +1,19 @@
 # import telegram_botapi
 import storage
+import pymorphy2
+import re
 
 def handle_incoming_message(text, is_keyboard, sender_id): 
     print("TODO")
+
+def split_string(string):
+	words = re.split('\W+', string)
+	words.remove('')
+	return_list = []
+	morph = pymorphy2.MorphAnalyzer()
+	for i in range(0, len(words)):
+		return_list.append(morph.parse(words[i])[0].normal_form)
+	return return_list
 
 # Some examples of Storage API usage
 print(storage.fetch_questions()[0].answer["type"])
