d5ba74c189008fe4e4e4143d0c170d0d6c958c4d
ae.popov <ae.popov@tensor.ru>
2016-11-19 11:51:28 +0300

    Дескрипторы файлов надо закрывать

--- jo_questions.py
+++ jo_questions.py
@@ -16,9 +16,13 @@ class JOQuestion:
 
 questions = {}
 
-dct = json.load(open("jo-questions.json", "r", encoding="utf-8"))
-for name, sdct in dct.items():
-    questions[name] = JOQuestion(name, sdct)
-for name, q in questions.items():
-    for answ in q.answers:
-        answ.next_question = questions[answ.next_question]
+dct = None
+with open("jo-questions.json", "r", encoding="utf-8") as q:
+    dct = json.load(q)
+
+if dct is not None:
+    for name, sdct in dct.items():
+        questions[name] = JOQuestion(name, sdct)
+    for name, q in questions.items():
+        for answ in q.answers:
+            answ.next_question = questions[answ.next_question]
