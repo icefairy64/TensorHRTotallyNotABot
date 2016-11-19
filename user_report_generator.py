32f511eccc0bf2018a2d885d28c19baa467d5575
severyanov <aa.severyanov@tensor.ru>
2016-11-19 21:11:34 +0300

    Генерация html-отчёта

--- user_report_generator.py
+++ user_report_generator.py
@@ -3,7 +3,32 @@
 import storage
 import codecs
 
+
 def generate_report_for_user(user, out_filename):
-    with codecs.open("report_template.txt", "r", encoding="utf-8") as rdr:
-        with codecs.open(out_filename, "w", encoding="utf-8") as wtr:
-            wtr.writelines(u'\n'.join([x for x in rdr.readlines()]).format(user.name, user.age, user.learn_exp, user.work_exp, user.skills))
+    template_file = codecs.open('report_template.html', 'r', 'utf-8')
+    template = template_file.read()
+    template_file.close()
+
+    report = template.format(
+        name=user.name,
+        age=user.age,
+        learn_exp=user.learn_exp,
+        work_exp=user.work_exp,
+        skills=user.skills
+    )
+
+    with codecs.open(out_filename, "w", encoding="utf-8") as report_file:
+        report_file.write(report)
+
+if __name__ == '__main__':
+    from storage import User
+
+    generate_report_for_user(User(1,
+                                  '1',
+                                  u'Василий Пупкин',
+                                  25,
+                                  u'Закончил КГТУ',
+                                  u'Нигде ещё не работал, тольк отучился',
+                                  u'C# и разработка игр',
+                                  u'программист c#'),
+                             'report.html')
