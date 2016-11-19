e4fb895fca2b02dcea2396b6de9a774d9ea1e36c
ae.popov <ae.popov@tensor.ru>
2016-11-19 16:42:33 +0300

    Оценка вопросов теста (простые типы)

--- quiz/answer_evaluation.py
+++ quiz/answer_evaluation.py
@@ -32,3 +32,63 @@ class AnswerEvaluation(object):
         """
         raise NotImplementedError(
             'Необходимо реализовать метод estimate (оценить)')
+
+class AnswerEvaluationSingleChoice(AnswerEvaluation):
+
+    def estimate(self, answer, reference, infelicity=0):
+        answer = answer.lower()
+        reference = reference.lower()
+        if infelicity > 0:
+            # Вычисляем разницу в словах
+            dam_lev_dis = damerau_levenstein_distance(answer, reference)
+            p = dam_lev_dis / len(reference)
+            if p < infelicity:
+                # Формирование оценки
+                return round(1 - p, 2)
+            else:
+                return 0
+
+        if answer == reference:
+            return 1
+        else:
+            return 0
+
+
+class AnswerEvaluationMultipleChoice(AnswerEvaluation):
+
+    def estimate(self, answer, reference, infelicity=0):
+        if answer is None or reference is None:
+            return 0
+        result = 0
+
+        ref_inf = reference.get('infelicity', 0)
+
+        if infelicity > 0:
+            for ra in reference['answers']:
+                for aa in answer:
+                    dam_lev_dis = damerau_levenstein_distance(aa, ra)
+                    p = dam_lev_dis / len(ra)
+                    if p < infelicity:
+                        result += 1
+        else:
+            for ra in reference['answers']:
+                if ra in answer:
+                    result += 1
+
+        if ref_inf == 0:
+            if result == len(reference['answers']):
+                return 1
+            else:
+                return 0
+
+        if result < ref_inf:
+            return 0
+        else:
+            return round(result / len(reference['answers']), 2)
+
+
+class AnswerEvaluationFreeForm(AnswerEvaluation):
+
+    def estimate(self, answer, reference):
+        return 1
+
